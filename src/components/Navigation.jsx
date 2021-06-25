66cfab4bf5335a4664512a810e5c89fdf4da8fc5
phuocleoceo <ht10082001@gmail.com>
2021-06-26 00:11:30 +0700

    NavLink

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -13,18 +13,18 @@ export default function Navigation() {
 						Home
 					</NavLink>
 
-					<Dropdown className="d-inline p-2 bg-dark text-white">
+					<Dropdown>
 						<Dropdown.Toggle id="dropdown-basic">
 							Dạng bài tập
 						</Dropdown.Toggle>
 
 						<Dropdown.Menu>
-							<Dropdown.Item href="/format1">Kim loại tác dụng HCl/H2SO4</Dropdown.Item>
-							<Dropdown.Item href="/format2">CO2 tác dụng dung dịch kiềm</Dropdown.Item>
-							<Dropdown.Item href="/format3">Hỗn hợp Oxit sắt tác dụng HNO3</Dropdown.Item>
-							<Dropdown.Item href="/format4">Al3+ tác dụng OH-</Dropdown.Item>
-							<Dropdown.Item href="/format5">AlO2- tác dụng H+</Dropdown.Item>
-							<Dropdown.Item href="/format6">H+ tác dụng hỗn hợp HCO3- và (CO3)2-</Dropdown.Item>
+							<Dropdown.Item><NavLink to="/format1">Kim loại tác dụng HCl/H2SO4</NavLink></Dropdown.Item>
+							<Dropdown.Item><NavLink to="/format2">CO2 tác dụng dung dịch kiềm</NavLink></Dropdown.Item>
+							<Dropdown.Item><NavLink to="/format3">Hỗn hợp Oxit sắt tác dụng HNO3</NavLink></Dropdown.Item>
+							<Dropdown.Item><NavLink to="/format4">Al3+ tác dụng OH-</NavLink></Dropdown.Item>
+							<Dropdown.Item><NavLink to="/format5">AlO2- tác dụng H+</NavLink></Dropdown.Item>
+							<Dropdown.Item><NavLink to="/format6">H+ tác dụng hỗn hợp HCO3- và (CO3)2-</NavLink></Dropdown.Item>
 						</Dropdown.Menu>
 					</Dropdown>
 
