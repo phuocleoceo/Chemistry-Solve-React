b503610917b3ad8b17123f4453aa2db1d1d55578
phuocleoceo <ht10082001@gmail.com>
2021-06-26 10:55:50 +0700

    Complete NavBar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 export default function Navigation() {
@@ -8,20 +8,56 @@ export default function Navigation() {
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav>
-
-					<NavLink className="d-inline p-2 text-white" to="/">
+					<Nav.Link as={Link} className="d-inline p-2 text-white" to="/">
+						<img
+							src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Creative-Tail-chemical.svg/1200px-Creative-Tail-chemical.svg.png"
+							width="30"
+							height="30"
+							className="d-inline-block align-top"
+							alt="Logo"
+						/>
+						&nbsp;
 						Trang chủ
-					</NavLink>
+					</Nav.Link>
 
-					<NavDropdown title="Dạng bài tập" className="nav-down" id="basic-nav-dropdown">
-						<NavDropdown.Item><NavLink to="/format1">Kim loại tác dụng HCl/H2SO4</NavLink></NavDropdown.Item>
-						<NavDropdown.Item><NavLink to="/format2">CO2 tác dụng dung dịch kiềm</NavLink></NavDropdown.Item>
-						<NavDropdown.Item><NavLink to="/format3">Hỗn hợp Oxit sắt tác dụng HNO3</NavLink></NavDropdown.Item>
-						<NavDropdown.Item><NavLink to="/format4">Al3+ tác dụng OH-</NavLink></NavDropdown.Item>
-						<NavDropdown.Item><NavLink to="/format5">AlO2- tác dụng H+</NavLink></NavDropdown.Item>
-						<NavDropdown.Item><NavLink to="/format6">H+ tác dụng hỗn hợp HCO3- và (CO3)2-</NavLink></NavDropdown.Item>
+					<NavDropdown title={<span className="text-white">Dạng bài tập</span>}
+						className="nav-down" id="basic-nav-dropdown">
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/format1" className="d-inline p-2 text-dark">
+								Kim loại tác dụng HCl/H2SO4
+							</Nav.Link>
+						</NavDropdown.Item>
+
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/format2" className="d-inline p-2 text-dark">
+								CO2 tác dụng dung dịch kiềm
+							</Nav.Link>
+						</NavDropdown.Item>
+
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/format3" className="d-inline p-2 text-dark">
+								Hỗn hợp Oxit sắt tác dụng HNO3
+							</Nav.Link>
+						</NavDropdown.Item>
+
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/format4" className="d-inline p-2 text-dark">
+								Al3+ tác dụng OH-
+							</Nav.Link>
+						</NavDropdown.Item>
+
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/format5" className="d-inline p-2 text-dark">
+								AlO2- tác dụng H+
+							</Nav.Link>
+						</NavDropdown.Item>
+
+						<NavDropdown.Item>
+							<Nav.Link as={Link} to="/format6" className="d-inline p-2 text-dark">
+								H+ tác dụng hỗn hợp HCO3- và (CO3)2-
+							</Nav.Link>
+						</NavDropdown.Item>
 					</NavDropdown>
-
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar >
