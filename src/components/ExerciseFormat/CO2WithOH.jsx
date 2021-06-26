dac28427477cc7f52515af62fa1a9cb7b470b3f4
phuocleoceo <ht10082001@gmail.com>
2021-06-26 21:58:14 +0700

    Format2 Calculate

--- src/components/ExerciseFormat/CO2WithOH.jsx
+++ src/components/ExerciseFormat/CO2WithOH.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import { Button, Row, Col, Form, Container } from 'react-bootstrap';
 
 export default function CO2WithOH() {
-	const [comment, setComment] = useState("");
 	const [result, setResult] = useState({
+		comment: "",
 		Na_Plus: 0, K_Plus: 0, Ba_2Plus: 0, Ca_2Plus: 0,
 		HCO3_Minus: 0, CO3_2Minus: 0,
 		OH_Minus_Remnant: 0, CO2_Remnant: 0
@@ -11,6 +11,62 @@ export default function CO2WithOH() {
 
 	function handleSubmit(e) {
 		e.preventDefault();
+		const molCO2 = parseFloat(e.target.CO2Volume.value) / 22.4;
+		const VBazo = parseFloat(e.target.BazoVolume.value);
+		const molNaOH = parseFloat(e.target.CMNaOH.value) * VBazo;
+		const molKOH = parseFloat(e.target.CMKOH.value) * VBazo;
+		const molBaOH2 = parseFloat(e.target.CMBaOH2.value) * VBazo;
+		const molCaOH2 = parseFloat(e.target.CMCaOH2.value) * VBazo;
+		const molOH = molNaOH + molKOH + molBaOH2 * 2 + molCaOH2 * 2;
+		const T = molOH / molCO2;
+		// calculate
+		const NaPlus = molNaOH;
+		const KPlus = molKOH;
+		const Ba2Plus = molBaOH2;
+		const Ca2Plus = molCaOH2;
+		let commentValue = "";
+		let OHMinusRemnant = 0;
+		let CO2Remnant = 0;
+		let HCO3Minus = 0;
+		let CO32Minus = 0;
+		//OH- + CO2 => HCO3- , OH- hết
+		if (T <= 1) {
+			commentValue = "Phản ứng chỉ tạo HCO3-, không kết tủa";
+			OHMinusRemnant = 0;
+			CO2Remnant = molCO2 - molOH;
+			HCO3Minus = molOH;
+			CO32Minus = 0;
+		}
+		//OH- + CO2 => HCO3-
+		//2OH- + CO2 => CO3(2-) + H2O
+		else if (T < 2) {
+			commentValue = "Phản ứng tạo hỗn hợp HCO3- và (CO3)2-";
+			OHMinusRemnant = 0;
+			CO2Remnant = 0;
+			const molCO3 = molOH - molCO2;
+			HCO3Minus = molCO3;
+			CO32Minus = molCO2 - molCO3;
+		}
+		// 2OH- + CO2 => CO3(2-) + H2O
+		else {
+			commentValue = "Phản ứng chỉ tạo (CO3)2-";
+			OHMinusRemnant = molOH - molCO2 * 2;
+			CO2Remnant = 0;
+			HCO3Minus = 0;
+			CO32Minus = molCO2;
+		}
+
+		setResult({
+			comment: commentValue,
+			Na_Plus: NaPlus,
+			K_Plus: KPlus,
+			Ba_2Plus: Ba2Plus,
+			Ca_2Plus: Ca2Plus,
+			OH_Minus_Remnant: OHMinusRemnant,
+			CO2_Remnant: CO2Remnant,
+			HCO3_Minus: HCO3Minus,
+			CO3_2Minus: CO32Minus
+		});
 	}
 
 	return (
@@ -70,7 +126,7 @@ export default function CO2WithOH() {
 
 						<Form.Group controlId="SaltMass" style={{ marginTop: '7%' }}>
 							<Form.Label>Kết quả phản ứng</Form.Label>
-							<Form.Control value={comment} type="text" name="comment" plaintext required readOnly
+							<Form.Control value={result.comment} type="text" name="comment" required readOnly
 								placeholder="Nhận xét kết quả..." />
 						</Form.Group>
 
