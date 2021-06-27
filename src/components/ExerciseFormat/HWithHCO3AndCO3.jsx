b6f53fbfac70f06b9720376a78ba6c7f1ead3a3d
phuocleoceo <ht10082001@gmail.com>
2021-06-27 10:49:54 +0700

    Format 6 UI

--- src/components/ExerciseFormat/HWithHCO3AndCO3.jsx
+++ src/components/ExerciseFormat/HWithHCO3AndCO3.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Button, Row, Col, Form, Container } from 'react-bootstrap';
 
 export default function HWithHCO3AndCO3() {
-	return (
-		<div>
+	const [result, setResult] = useState({
+		comment: "", CO2: 0, HPlus_Remnant: 0,
+		HCO3_Minus_Remnant: 0, CO3_2Minus_Remnant: 0
+	});
 
-		</div>
+	function handleSubmit(e) {
+		e.preventDefault();
+	}
+	return (
+		<Container style={{ marginTop: '1%' }}>
+			<Row>
+
+				<Form onSubmit={handleSubmit}>
+					<Form.Label className="font-weight-bold text-success">Trước phản ứng :</Form.Label>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="HCO3Minus">
+							<Form.Label>HCO3- (mol)</Form.Label>
+							<Form.Control type="text" name="HCO3Minus" required
+								placeholder="Nhập số mol HCO3-..." />
+						</Form.Group>
+
+						<Form.Group as={Col} controlId="CO32Minus">
+							<Form.Label>(CO3)2- (mol)</Form.Label>
+							<Form.Control type="text" name="CO32Minus" required
+								placeholder="Nhập số mol (CO3)2-..." />
+						</Form.Group>
+					</Form.Row>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="HPlus">
+							<Form.Label>H+ (mol)</Form.Label>
+							<Form.Control type="text" name="HPlus" required
+								placeholder="Nhập số mol H+..." />
+						</Form.Group>
+
+						<Form.Group as={Col} controlId="Axit">
+							<Form.Label>Chọn trường hợp</Form.Label>
+							<Form.Control name="Axit" as="select">
+								<option value="1">Đổ H+ vào HCO3- và (CO3)2-</option>
+								<option value="2">Đổ HCO3- và (CO3)2- vào H+</option>
+							</Form.Control>
+						</Form.Group>
+					</Form.Row>
+
+					<Form.Group>
+						<Button variant="primary" type="submit" block>
+							Tính toán
+						</Button>
+					</Form.Group>
+				</Form>
+
+			</Row >
+			<hr />
+			<Row>
+				<Col sm={6}>
+					<Form.Label className="font-weight-bold text-success">Sau phản ứng :</Form.Label>
+
+					<Form.Group controlId="Comment">
+						<Form.Label>Kết quả phản ứng</Form.Label>
+						<Form.Control value={result.comment} type="text" plaintext required readOnly
+							placeholder="Nhận xét kết quả..." />
+					</Form.Group>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="CO2">
+							<Form.Label>CO2 (mol)</Form.Label>
+							<Form.Control value={result.CO2} type="text" plaintext required readOnly />
+						</Form.Group>
+
+						<Form.Group as={Col} controlId="HPlusRemnant">
+							<Form.Label>H+ dư (mol)</Form.Label>
+							<Form.Control value={result.HPlus_Remnant} type="text" plaintext required readOnly />
+						</Form.Group>
+					</Form.Row>
+
+					<Form.Row>
+						<Form.Group as={Col} controlId="HCO3MinusRemnant">
+							<Form.Label>HCO3- dư (mol)</Form.Label>
+							<Form.Control value={result.HCO3_Minus_Remnant} type="text" plaintext required readOnly />
+						</Form.Group>
+
+						<Form.Group as={Col} controlId="CO32MinusRemnant">
+							<Form.Label>(CO3)2- dư (mol)</Form.Label>
+							<Form.Control value={result.CO3_2Minus_Remnant} type="text" plaintext required readOnly />
+						</Form.Group>
+					</Form.Row>
+				</Col>
+
+				<Col sm={6}>
+
+				</Col>
+			</Row >
+		</Container >
 	)
 }
