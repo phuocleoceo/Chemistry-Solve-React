3f8b6a6729f648d565b24a579869f524595cb73f
phuocleoceo <ht10082001@gmail.com>
2021-07-08 17:25:29 +0700

    Nav.Link is unnecessary

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 
 export default function Navigation() {
 	return (
@@ -9,7 +9,7 @@ export default function Navigation() {
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav>
 					<Nav.Link as={Link} className="d-inline p-2 text-white" to="/">
-						<img
+						<Image
 							src="/img/icon.png"
 							width="30"
 							height="30"
@@ -22,57 +22,40 @@ export default function Navigation() {
 
 					<NavDropdown title={<span className="text-white">Dạng bài tập vô cơ</span>}
 						className="nav-down" id="basic-nav-dropdown">
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format1" className="d-inline p-2 text-dark">
-								Kim loại tác dụng HCl/H2SO4
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format1">
+							Kim loại tác dụng HCl/H2SO4
 						</NavDropdown.Item>
 
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format2" className="d-inline p-2 text-dark">
-								CO2 tác dụng dung dịch kiềm
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format2">
+							CO2 tác dụng dung dịch kiềm
 						</NavDropdown.Item>
 
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format3" className="d-inline p-2 text-dark">
-								Hỗn hợp Oxit sắt tác dụng HNO3
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format3">
+							Hỗn hợp Oxit sắt tác dụng HNO3
 						</NavDropdown.Item>
 
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format4" className="d-inline p-2 text-dark">
-								Al3+ tác dụng OH-
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format4">
+							Al3+ tác dụng OH-
 						</NavDropdown.Item>
 
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format5" className="d-inline p-2 text-dark">
-								AlO2- tác dụng H+
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format5">
+							AlO2- tác dụng H+
 						</NavDropdown.Item>
 
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format6" className="d-inline p-2 text-dark">
-								H+ tác dụng hỗn hợp HCO3- và (CO3)2-
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format6">
+							H+ tác dụng hỗn hợp HCO3- và (CO3)2-
 						</NavDropdown.Item>
 					</NavDropdown>
 
 					<NavDropdown title={<span className="text-white">Dạng bài tập hữu cơ</span>}
 						className="nav-down" id="basic-nav-dropdown">
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format7" className="d-inline p-2 text-dark">
-								Lên men glucozơ
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format7">
+							Lên men glucozơ
 						</NavDropdown.Item>
 
-						<NavDropdown.Item>
-							<Nav.Link as={Link} to="/format8" className="d-inline p-2 text-dark">
-								Tìm CTCT từ phản ứng đốt cháy
-							</Nav.Link>
+						<NavDropdown.Item as={Link} to="/format8">
+							Tìm CTCT từ phản ứng đốt cháy
 						</NavDropdown.Item>
-
 					</NavDropdown>
 				</Nav>
 			</Navbar.Collapse>
